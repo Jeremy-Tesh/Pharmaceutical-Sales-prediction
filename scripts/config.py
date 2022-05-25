0a478caf04e4bebc8ef9b4c4a59a4678509853b2
Jeremy Tesh <jermitesh20@gmail.com>
2022-05-25 21:56:50 +0300

    feat:eda and cml

--- scripts/config.py
+++ scripts/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 class Config:
   RANDOM_SEED = 27
   ASSETS_PATH = Path("../")
-  REPO = "https://github.com/eandualem/PharmaceuticalSalesPrediction"
+  REPO = "https://github.com/Jeremy-Tesh/Pharmaceutical-Sales-prediction"
   STORE_PATH = ASSETS_PATH / "data/store.csv"
   TRAIN_PATH = ASSETS_PATH / "data/train.csv"
   TEST_PATH = ASSETS_PATH / "data/test.csv"
