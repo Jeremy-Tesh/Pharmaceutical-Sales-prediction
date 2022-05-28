c18367ad00bd8ac348bfc694183c462d7b7d59ea
Jeremy Tesh <jermitesh20@gmail.com>
2022-05-28 22:39:36 +0300

    feat:dashboard

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
+import os
+import sys
 import streamlit as st
+sys.path.append(os.path.abspath(os.path.join('./pages')))
+import home
+import data
+import insights
+import prediction
 
-st.title("Rossmann-Pharmaceuticals-Sales-Prediction")
-st.subheader("welcome")
+
+PAGES = {
+  "Home": home,
+  "Data": data,
+  "Insights": insights,
+  "Prediction": prediction
+}
+
+selection = st.sidebar.radio("Go to page", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
